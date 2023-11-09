2b11296da06609558c5eefa168139f7a90969847
Xavier Pashley <xavipashley@gmail.com>
2023-11-09 13:55:21 +0000

    MESSAGEY

---
+++ Day4/objects.js
@@ -0,0 +1,92 @@
+// let person = {
+//     name: "Xavi",
+//     age: 21
+// };
+// console.log(person);
+
+
+
+
+// let offer = "none";
+// let time = 1500;
+
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     hasSpecialOffers: true,
+//     drinks: ["Cappuccino", "Latter", "Filter coffee", "Tea", "Hot chocolate"],
+//     lunchOffer: "Free drink with suprisingly priced sandwich",
+//     breakfastOffer: "Free croisants with coffee",
+//     noOffer: "Sorry no offer",
+// };
+// if (time < 110) {
+//     offer = cafe.breakfastOffer;
+//     console.log(cafe.breakfastOffer);
+// } else if (time < 1500) {
+//     offer= cafe.luchOffer;
+//     console.log(cafe.lunchOffer);
+// }
+
+
+
+
+// let offer = "none";
+// let time = 12700;
+ 
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     hasSpecialOffers: true,
+//     drinks: ["Cappuccino", "Latte", "Filter Coffee", "Tea", "Hot Chocolate"],
+//     breakfastOffer: "Free croissant with coffee",
+//     lunchOffer: "Free drink with surprisingly priced sandwich",
+//     noOffer: "Sorry - no offer",
+   
+//     openCafe(){
+//         if (this.hasSpecialOffers){
+//             return "Time for a special offer";
+//         }
+//     },
+ 
+//     closeCafe(){
+//         return "We are closed. Come back tomorrow";
+//     }
+// };
+ 
+// console.log(cafe.openCafe());
+
+
+
+
+
+// let day = "Monday"
+// const alarm = {
+//     weekendAlarm: "No alarm needed.",
+//     weekdayAlarm: "Get up at 7AM",
+// };
+// if (day == "Saturday") {
+//     console.log(alarm.weekendAlarm)
+// }    else if (day == "Sunday") {
+//     console.log(alarm.weekendAlarm)
+// }        else{
+//     console.log(alarm.weekdayAlarm)
+// }
+
+
+
+
+
+// [11:28] Brian Harkins
+// let person = {
+//     name: "Brian",
+//     age: 38,
+//     favSongs: ["Oops I did it again - Britney", "Hysteria - Muse", "Sleeptalking - Level 42"]
+// };
+ 
+// person.favSongs[2] = "Jigsaw - Radiohead";
+// console.log(person);
+
+
+
+
+
